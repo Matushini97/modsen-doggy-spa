282ee74ee197c0e777c2594f7f86361e675326e1
Ilya Matsiushyn <matushin.97@gmail.com>
2024-05-29 12:07:00 +0300

    feat: header link underline

--- src/components/ui/layout/nav-links/nav-links.tsx
+++ src/components/ui/layout/nav-links/nav-links.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { HeaderLinks } from "@/constants/links";
 import s from "./nav-links.module.scss";
-import { link } from "fs";
 import { Button } from "../../button";
 import Link from "next/link";
 import clsx from "clsx";
@@ -13,13 +12,11 @@ export const NavLinks = () => {
       {HeaderLinks.map((link) => {
         return (
           <Button
+            className={clsx(pathname === link.href && s.activeLink)}
             as={Link}
             href={link.href}
             variant="link"
             key={link.name}
-            className={clsx({
-              'activeLink': pathname === link.href,
-            })}
           >
             {link.name}
           </Button>
